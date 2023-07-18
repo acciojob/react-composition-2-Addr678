743cefd411e883214c3d9d866b26659d84a3f90e
Addr678 <im_yadavprashantkumar33@gmail.com>
2023-07-18 08:16:27 +0530

    add

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,36 @@
 
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+
+const Modal = ({ show, onClose, children }) => {
+  return (
+    <div className={`modal-overlay ${show ? "show" : ""}`}>
+      <div className="modal-dialog">
+        <button className="modal-close" onClick={onClose}>
+          Close
+        </button>
+        <div className="modal-content">{children}</div>
+      </div>
+    </div>
+  );
+};
 
 const App = () => {
+  const [showModal, setShowModal] = React.useState(false);
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      <button onClick={() => setShowModal(true)}>Open Modal</button>
 
-export default App
+      <Modal show={showModal} onClose={handleCloseModal}>
+        <p className="modal-p">Modal Content</p>
+      </Modal>
+    </div>
+  );
+};
+
+export default App;
